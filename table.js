ac261f0cec798b4018eaae01ebb177a542ee9c52
BenitoIshimwe <benishimwe31@gmail.com>
2023-01-09 02:39:29 +0100

    Add Unique constraint

--- table.js
+++ table.js
@@ -8,13 +8,13 @@ const db = new sqlite.Database(
 );
 
 const Mysql = {
-  sql1: `CREATE TABLE users(id INTEGER PRIMARY KEY AUTOINCREMENT, name VARCHAR(32) NOT NULL, email VARCHAR(64) NOT NULL, password VARCHAR(32) NOT NULL)`,
+  sql1: `CREATE TABLE users(id INTEGER PRIMARY KEY AUTOINCREMENT, name VARCHAR(32) NOT NULL UNIQUE, email VARCHAR(64) NOT NULL UNIQUE, password TEXT NOT NULL)`,
   sql2: `PRAGMA foreign_keys = ON`,
   sql3: `CREATE TABLE tweets(id INTEGER PRIMARY KEY AUTOINCREMENT, author INTEGER NOT NULL, content TEXT NOT NULL, createdAt TEXT NOT NULL, FOREIGN KEY(author) REFERENCES users(id))`,
-  sql4: `CREATE TABLE friends_list(id INTEGER PRIMARY KEY AUTOINCREMENT, friend_id INTEGER NOT NULL, friend_name VARCHAR(32) NOT NULL, FOREIGN KEY(friend_id, friend_name) REFERENCES users(id, name))`,
+  sql4: `CREATE TABLE friends(friend1_id INTEGER PRIMARY KEY AUTOINCREMENT, friend2_id INTEGER NOT NULL, FOREIGN KEY(friend2_id) REFERENCES users(id))`,
 };
 
-Object.entries(Mysql).forEach(([key, value]) => {
-  db.run(value);
+Object.entries(Mysql).forEach(async ([key, value]) => {
+  await db.run(value);
   console.log("Run the sql", key);
 });
